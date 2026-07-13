7ca3e8fd44348c6a0d1b6bb4ac17ac4e1a080694
gayathri <gayathrichowdary441@gmail.com>
2026-07-13 12:53:59 +0530

    main

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+echo "===== BUILD START ====="
+
 apt-get update
+
 apt-get install -y tesseract-ocr
 
+which tesseract
+
+tesseract --version
+
 pip install -r requirements.txt
+
+echo "===== BUILD COMPLETE ====="
